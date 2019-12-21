79339f5fbe614f221f54d71c94d3c7f81615180b
Chenhongxu <705105278@qq.com>
2019-12-21 13:24:00 +0800

    server test

--- main.go
+++ main.go
@@ -1,5 +1,14 @@
+// main.go文件
 package main
 
+import (
+	"net/http"
+)
+
 func main() {
-	println("hello world")
+	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+		writer.Write([]byte("Here is the home page."))
+	})
+
+	http.ListenAndServe(":8080", nil)
 }
